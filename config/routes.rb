0760d79c8d987079a78df6a30034fe8ea24ac758
delacruzjames <delacruzjamesmartin@gmail.com>
2012-08-11 15:31:25 +0800

    generate user model/delete defaultpages/generate pages controller/set up signup-register-signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ASI::Application.routes.draw do
+  devise_for :users
+
+  get 'pages/help'
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

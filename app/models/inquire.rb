f996cb2f0b282260d40241d79575b39ef7cbde45
delacruzjames <delacruzjamesmartin@gmail.com>
2012-08-12 15:17:42 +0800

    added nature and source attributes id on inquire model

--- app/models/inquire.rb
+++ app/models/inquire.rb
@@ -1,5 +1,5 @@
 class Inquire < ActiveRecord::Base
-  attr_accessible :company_name
+  attr_accessible :company_name, :nature_of_business_id, :source_id
   belongs_to :user
   has_many :nature_of_businesses
   has_many :sources
